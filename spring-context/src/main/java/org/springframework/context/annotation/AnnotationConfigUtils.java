986aa6162d9d67f24aaceb9e7bc6e438bea159d6
schening <schening@outlook.com>
2022-06-24 00:48:44 +0800

    @Configuration解析过程

--- spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigUtils.java
+++ spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigUtils.java
@@ -161,6 +161,7 @@ public abstract class AnnotationConfigUtils {
 		Set<BeanDefinitionHolder> beanDefs = new LinkedHashSet<>(8);
 
 		if (!registry.containsBeanDefinition(CONFIGURATION_ANNOTATION_PROCESSOR_BEAN_NAME)) {
+			// ConfigurationClassPostProcessor 被封装成 Spring Bean 定义（BeanDefinition）后续注册为 Spring Bean
 			RootBeanDefinition def = new RootBeanDefinition(ConfigurationClassPostProcessor.class);
 			def.setSource(source);
 			beanDefs.add(registerPostProcessor(registry, def, CONFIGURATION_ANNOTATION_PROCESSOR_BEAN_NAME));
